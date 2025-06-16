f3a58086284ed66556ceb53cb26e13fc77a041c3
Prakash Samanta <druvils123@gmail.com>
2025-06-17 00:27:37 +0530

    Fixed: Task1_LandingPage no longer a submodule, correctly tracked as a directory

---
+++ Task1_LandingPage/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <div className="footer">
+            <h4>Terms & Conditions</h4>
+            <h4>Privacy Policy</h4>
+            <h1>Ai Foresty</h1>
+        </div>
+    </>
+  )
+}
+
+export default Footer
---
+++ Task1_LandingPage/src/layouts/SectionThree.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const SectionThree = () => {
+  return (
+    <>
+        
+        <section class="secThree">
+            <div class="startup-nextlvl">
+                <h1>Everything you need to take your Startup to the next level.</h1>
+                <h6>Access our exclusive network with perks worth over 550,000 and boost your startup</h6>
+                <div class="networks">
+                    <div class="network">
+                    <div class="logo"></div>
+                    <h3>Intercom</h3>
+                    <h4>S1000 en credit Notion Plus lon</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum odit quod adipisci!</p>
+                </div><div class="network">
+                    <div class="logo"></div>
+                    <h3>Intercom</h3>
+                    <h4>S1000 en credit Notion Plus lon</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum odit quod adipisci!</p>
+                </div><div class="network">
+                    <div class="logo"></div>
+                    <h3>Intercom</h3>
+                    <h4>S1000 en credit Notion Plus lon</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum odit quod adipisci!</p>
+                </div><div class="network">
+                    <div class="logo"></div>
+                    <h3>Intercom</h3>
+                    <h4>S1000 en credit Notion Plus lon</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum odit quod adipisci!</p>
+                </div>
+
+                </div>
+                
+            </div>
+            <div className="founder-mssg">
+                <h1>""</h1>
+                <video src=""></video>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default SectionThree
