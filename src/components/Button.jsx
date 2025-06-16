9a23a3fa11a14b159bbf9cd38d20f7c603727b19
Prakash Samanta <druvils123@gmail.com>
2025-06-16 23:31:12 +0530

    Initial commit: Responsive Landing Page for OIBSIP Internship

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+ 
+
+const Button = ({text}) => {
+  return (
+    <>
+      <div className="cta-btn w-fit p-2 text-white rounded bg-[#104026]">{text}</div>
+    </>
+  )
+}
+
+export default Button
