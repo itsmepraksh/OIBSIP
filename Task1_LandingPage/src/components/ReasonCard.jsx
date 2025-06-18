2238628de640cb039ca3865dc47f47fcc6848aca
Prakash Samanta <druvils123@gmail.com>
2025-06-19 00:58:29 +0530

    Day3 Completed Section-2 styling and responsive part..

--- Task1_LandingPage/src/components/ReasonCard.jsx
+++ Task1_LandingPage/src/components/ReasonCard.jsx
@@ -4,14 +4,14 @@ import Button from "./Button";
 
 const ReasonCard = ({text}) => {
   return (
-    <div className="reason bg-white shrink-[0] w-[80%] aspect-1/1  px-[2rem] md:px-[4rem] lg:px-[6rem] xl:px-[9rem] 2xl:px-[12rem] py-[2rem] md:py-[3rem] ">
+    <div className="reason bg-white shrink-[0] w-[80%] md:w-[45%] lg:w-[30%] aspect-1/1 lg:aspect-3/1  px-[2rem]  md:px-[3rem] lg:px-[2.5rem]   py-[2rem] md:py-[3rem] ">
       <FontAwesomeIcon className="text-[#104026] mb-2 text-lg" icon={faGlobe} />
       <h4 className="font-medium">{text}</h4>
       <small className=" block leading-[1.3] py-3 font-medium text-gray-400 text-xs">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit officiis
         asperiores exercitationem? Fuga neque minus at exercitationem nobis.
       </small>
-      <Button className="w-[55%] my-4   xl:py-3 xl:px-4  " text="Details" />
+      <Button className=" w-[55%] xl:w-[45%] xl:text-center  my-4   xl:py-3 xl:px-4  " text="Details" />
     </div>
   );
 };
--- Task1_LandingPage/src/layouts/SectionTwo.jsx
+++ Task1_LandingPage/src/layouts/SectionTwo.jsx
@@ -1,21 +1,20 @@
-import { faGlobe } from "@fortawesome/free-solid-svg-icons";
-import Button from "../components/Button";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+ 
+import Button from "../components/Button"; 
 import ReasonCard from "../components/ReasonCard";
 
 const SectionTwo = () => {
   return (
     <>
-      <section className="secTwo">
-        <div className="why-member">
+      <section className="secTwo flex flex-col justify-self-center items-center px-[2rem] md:px-[4rem] lg:px-[6rem] xl:px-[9rem] 2xl:px-[12rem] py-[1rem] md:py-[3rem]  ">
+        <div className="why-member flex flex-col justify-center items-center">
           <h1 className="text-2xl md:text-3xl lg:text-4xl  md:w-[80%] lg:w-[65%] xl:w-[55%]     font-bold text-[#133C2A] text-center py-5 md:py-7">
             Why Become a Member?
           </h1>
-          <p className="text-xs text-gray-500 font-medium text-center pb-5 md:pb-10  md:w-[75%] lg:w-[55%] xl:w-[40%] ">
+          <p className="text-xs text-gray-500 font-medium text-center pb-5 md:pb-10  md:w-[55%] lg:w-[55%] xl:w-[40%] ">
             Experience the benefits of being part of a vibrant Al startup
             community that fosters collaboration and innovation.
           </p>
-          <div className="reasons-box  flex flex-col lg:flex-wrap items-center  gap-2 px-[2rem] md:px-[4rem] lg:px-[6rem] xl:px-[9rem] 2xl:px-[12rem] py-[1rem] md:py-[3rem] ">
+          <div className="reasons-box my-4 flex flex-col md:flex-row  md:flex-wrap items-center  justify-center gap-2 md:gap-4 ">
             <ReasonCard text="Networking" />
             <ReasonCard text="Exclusive Resources" />
             <ReasonCard text="Mentorship Programs" />
@@ -24,18 +23,18 @@ const SectionTwo = () => {
             <ReasonCard text="Monthly Meetups" />
           </div>
         </div>
-        <div className="raise-funding">
-          <div className="funds-data">
-            <div className="invested">
-              $30,000 <h5>invested!</h5>
+        <div className="raise-funding py-10 px-4 flex flex-col md:flex-row justify-around  xl:justify-evenly items-center ">
+          <div className="funds-data md:w-1/2 xl:w-[35%] ">
+            <div className="invested flex items-center justify-between -rotate-[3deg] gap-4 w-[95%] py-2 xl:py-4 px-8  text-white text-base xl:text-lg font-medium rounded-md my-4 bg-[#104026] md:w-[80%]  ">
+              $30,000 <h5 className="text-xs xl:text-sm font-normal">Invested!</h5>
             </div>
-            <div className="investing">
-              $100,000 <h5>investing</h5>
+            <div className="investing flex items-center justify-between  gap-4 w-[95%] xl:w-[80%] py-2 xl:py-4  px-8   text-base xl:text-lg font-medium rounded-md my-4 bg-white md:w-[80%]  ">
+              $100,000 <h5 className="text-gray-400 text-xs xl:text-sm font-normal">Investing</h5>
             </div>
           </div>
-          <div className="funding-info">
-            <h1>Raise Funding for Your Startup</h1>
-            <p>
+          <div className="funding-info md:w-1/2 ">
+            <h1  className="text-2xl text-left md:text-3xl lg:text-4xl  md:w-[80%] lg:w-[75%] xl:w-[65%]     font-bold text-[#133C2A] text-center py-5 md:py-7">Raise Funding for Your Startup</h1>
+            <p className="text-xs text-gray-500 text-left font-medium text-center pb-5 md:pb-10  md:w-[75%] lg:w-[70%] xl:w-[60%]  ">
               Launch a funding round through our online platform, allowing
               individuals to invest smaller amounts. Tap into your friends and
               family network, engage your brand's community, and connect with
@@ -43,7 +42,7 @@ const SectionTwo = () => {
               growth.
             </p>
 
-            <Button className="w-[55%]   xl:py-3 xl:px-4  " text="Learn more" />
+            <Button className="w-[55%] xl:w-[25%] xl:text-center xl:py-3 xl:px-4  " text="Learn more" />
           </div>
         </div>
       </section>
